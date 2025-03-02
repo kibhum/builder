db76fe56237e0b0593b6ae921a5ca3b0bddee6ef
kibethh <kibethh@gmail.com>
2025-03-02 05:46:00 +0300

    Added more functionality to the builder macro

--- builder-usage/src/main.rs
+++ builder-usage/src/main.rs
@@ -1,7 +1,5 @@
 use builder_macro::Builder;
 
-#[derive(Builder)]
-struct Gleipnir {}
 fn main() {
     println!("Hello, world!");
 }
@@ -15,5 +13,19 @@ mod tests {
         struct ExampleStructNoFields {}
 
         let _: ExampleStructNoFields = ExampleStructNoFields::builder().build();
+    }
+
+    #[test]
+    fn should_generate_builder_for_struct_with_one_property() {
+        #[derive(Builder)]
+        struct Gleipnir {
+            roots_of: String,
+        }
+
+        let gleipnir = Gleipnir::builder()
+            .roots_of("mountains".to_string())
+            .build();
+
+        assert_eq!(gleipnir.roots_of, "mountains".to_string());
     }
 }
