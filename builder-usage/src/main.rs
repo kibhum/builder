5b7cd43c3a3743cb765c71231669696dcef04805
kibethh <kibethh@gmail.com>
2025-03-04 00:45:47 +0300

    added blackbox and whitebox tests

--- builder-usage/src/main.rs
+++ builder-usage/src/main.rs
@@ -1,12 +1,10 @@
-use builder_macro::Builder;
-
 fn main() {
     println!("Hello, world!");
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    use builder_macro::Builder;
     #[test]
     fn should_generate_builder_for_struct_with_no_properties() {
         #[derive(Builder)]
@@ -18,14 +16,66 @@ mod tests {
     #[test]
     fn should_generate_builder_for_struct_with_one_property() {
         #[derive(Builder)]
-        struct Gleipnir {
+        struct GleipnirA {
             roots_of: String,
         }
 
-        let gleipnir = Gleipnir::builder()
+        let gleipnir = GleipnirA::builder()
             .roots_of("mountains".to_string())
             .build();
 
         assert_eq!(gleipnir.roots_of, "mountains".to_string());
+    }
+
+    #[test]
+    fn should_generate_builder_for_struct_with_two_properties() {
+        #[derive(Builder)]
+        struct GleipnirB {
+            roots_of: String,
+            breath_of_a_fish: u8,
+        }
+
+        let gleipnir = GleipnirB::builder()
+            .roots_of("mountain".to_string())
+            .breath_of_a_fish(1)
+            .build();
+
+        assert_eq!(gleipnir.roots_of, "mountain".to_string());
+        assert_eq!(gleipnir.breath_of_a_fish, 1);
+    }
+
+    #[test]
+    fn should_generate_builder_for_struct_with_multiple_properties() {
+        #[derive(Builder)]
+        struct GleipnirC {
+            roots_of: String,
+            breath_of_a_fish: u8,
+            other_necessities: Vec<String>,
+        }
+
+        let gleipnir = GleipnirC::builder()
+            .roots_of("mountain".to_string())
+            .breath_of_a_fish(1)
+            .other_necessities(vec![
+                "sound of a cat".to_string(),
+                "beard of a woman".to_string(),
+                "spittle of a bird".to_string(),
+            ])
+            .build();
+
+        assert_eq!(gleipnir.roots_of, "mountain".to_string());
+        assert_eq!(gleipnir.breath_of_a_fish, 1);
+        assert_eq!(gleipnir.other_necessities.len(), 3);
+    }
+
+    #[test]
+    #[should_panic]
+    fn should_panic_when_field_is_missing() {
+        #[derive(Builder)]
+        struct GleipnirD {
+            _roots_of: String,
+        }
+
+        GleipnirD::builder().build();
     }
 }
